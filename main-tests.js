f18bbab6b1ebe402a04ae0b383118e6ab76ae54e
Rosano Coutinho <pub@xeari.com>
2021-04-10 10:59:08 -0300

    Extract OLSKMomentPerceptionDay

--- main-tests.js
+++ main-tests.js
@@ -2,3 +2,30 @@ const { throws, deepEqual } = require('assert');
 
 const mod = require('./main.js');
 
+const offset = (function (inputData) {
+	return inputData < 10 ? `0${ inputData }` : inputData;
+})((new Date()).getTimezoneOffset() / 60);
+
+describe('OLSKMomentPerceptionDay', function test_OLSKMomentPerceptionDay() {
+
+	it('throws if not valid', function () {
+		throws(function () {
+			mod.OLSKMomentPerceptionDay(new Date('alfa'));
+		}, /OLSKErrorInputNotValid/);
+	});
+
+	it('returns day in current timezone', function () {
+		deepEqual(mod.OLSKMomentPerceptionDay(new Date(`2020-05-02T12:00:00-${ offset }:00`)), '2020-05-02');
+	});
+
+	it('previous day if before 4am', function () {
+		const date = new Date(`2020-05-02T03:59:00-${ offset }:00`);
+		deepEqual(mod.OLSKMomentPerceptionDay(date), '2020-05-01');
+	});
+
+	it('same day if 4am', function () {
+		const date = new Date(`2020-05-02T04:00:00-${ offset }:00`);
+		deepEqual(mod.OLSKMomentPerceptionDay(date), '2020-05-02');
+	});
+
+});
--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 const mod = {
 
+	OLSKMomentPerceptionDay (inputData) {
+		if (!(inputData instanceof Date) || Number.isNaN(inputData.getTime())) {
+			throw new Error('OLSKErrorInputNotValid');
+		}
+
+		return (new Date(inputData.valueOf() - (inputData.getTimezoneOffset() / 60 + 4) * 1000 * 60 * 60)).toJSON().slice(0, 10);
+	},
 
 };
 
